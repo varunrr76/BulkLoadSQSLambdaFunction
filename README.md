14ac7844dbb2ed5bcd295078d9b6b4ee0c16e21c
Ubuntu <ubuntu@ip-172-31-60-125.ec2.internal>
2024-02-21 08:51:13 +0000

    Initial commit for bulk-load-sqs-js-lambda-function

---
+++ README.md
@@ -0,0 +1,13 @@
+## BulkLoadSQSLambdaFunction
+
+This is a POC for Asynchronous JS Lambda Function using the following modules:
+
+1. "aws-sdk": "^2.472.0" 
+
+### Objective
+
+Push the messages into the sqs queue asynchronously in JS.
+
+### Setup
+
+Install packages with npm pacakge.json file.
---
+++ index.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+
+const QUEUE_URL = process.env('DEMO_QUEUE');
+
+function messagesGenerator(messagesCount) {
+  let messages = [];
+  for (let i = 1; i <= messagesCount; i++) {
+    messages.push(i.toString());
+  }
+  return messages;
+}
+
+exports.handler = async function (event, context) {
+
+  const messagesCount = event['messagesCount'] || 20;
+
+  const messages = messagesGenerator(messagesCount);
+
+  var sqs = new AWS.SQS();
+
+  const response = await Promise.all(messages.map((message) => sqs.sendMessage({ MessageBody: message, QueueUrl: QUEUE_URL }).promise()));
+
+  return response;
+}
+
